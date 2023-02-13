0deafaa4ea1a883309487827311962a02fc9dd0d
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-02-13 22:34:08 +0900

    During Lesson

--- Lesson/dist/app.js
+++ Lesson/dist/app.js
@@ -1,1 +1,11 @@
 "use strict";
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+    describe() {
+        console.log('Department: ' + this.name);
+    }
+}
+const accounting = new Department('Accounting');
+accounting.describe();
