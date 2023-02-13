dd0cee1baff258d8696cdeef948da6303c9bc2b8
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-02-14 06:05:05 +0900

    During Lesson

--- Lesson/dist/app.js
+++ Lesson/dist/app.js
@@ -1,2 +1,45 @@
 "use strict";
-console.log('Hello');
+const e1 = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date(),
+};
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log(emp.name);
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) {
+        console.log('startDate: ' + emp.startDate);
+    }
+}
+printEmployeeInformation({ name: 'Manu', startDate: new Date() });
+class Car {
+    drive() {
+        console.log('Driving Car now');
+    }
+}
+class Truck {
+    drive() {
+        console.log('Drivnig Truck now');
+    }
+    loadCargo(amount) {
+        console.log('Loaded with luggage...' + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- Lesson/src/app.ts
+++ Lesson/src/app.ts
@@ -1,1 +1,76 @@
-console.log('Hello')
+type Admin = {
+    name: string
+    privileges: string[]
+}
+
+type Employee = {
+    name: string
+    startDate: Date
+}
+
+// interface ElevatedEmoloyee extends Employee, Admin {}
+
+type ElevatedEmoloyee = Admin & Employee
+
+const e1: ElevatedEmoloyee = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date(),
+}
+
+type Combinable = string | number
+type Numeric = number | boolean
+
+type Universal = Combinable & Numeric
+
+function add(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString()
+    }
+    return a + b
+}
+
+type UnknownEmployee = Employee | Admin
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log(emp.name)
+    if ('privileges' in emp) {
+        console.log('Privileges: ' + emp.privileges)
+    }
+    if ('startDate' in emp) {
+        console.log('startDate: ' + emp.startDate)
+    }
+}
+
+printEmployeeInformation({ name: 'Manu', startDate: new Date() })
+
+class Car {
+    drive() {
+        console.log('Driving Car now')
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Drivnig Truck now')
+    }
+
+    loadCargo(amount: number) {
+        console.log('Loaded with luggage...' + amount)
+    }
+}
+
+type Vehicle = Car | Truck
+
+const v1 = new Car()
+const v2 = new Truck()
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive()
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000)
+    }
+}
+
+useVehicle(v1)
+useVehicle(v2)
