27f1578f8cb8069fee3918b0a6c7df35ca5d7b73
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-02-13 19:35:13 +0900

    During Lesson

---
+++ Lesson-File2/app.ts
@@ -0,0 +1,19 @@
+let appId = 'abc'
+const button = document.querySelector('button')! as HTMLButtonElement
+
+function add(n1: number, n2: number) {
+    if (n1 + n2 > 0) {
+        return n1 + n2
+    }
+    return
+}
+
+function clickHandler(message: string) {
+    // let userName = 'Max'
+    console.log('Clicked ' + message)
+}
+
+// a commnet
+if (button) {
+    button.addEventListener('click', clickHandler.bind(null, "Your welcome!"))
+}
