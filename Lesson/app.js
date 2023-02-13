2670fca55dc0b66700b51195aaab465cb6e20143
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-02-13 15:55:39 +0900

    During Lesson

---
+++ Lesson/app.js
@@ -0,0 +1,19 @@
+function add(n1, n2, showResult, phrase) {
+    if (typeof n1 !== "number" || typeof n2 !== "number") {
+        throw new Error("Missing Input Value");
+    }
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1;
+number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = "Result: ";
+// resultPhrase = 0
+add(number1, number2, printResult, resultPhrase);
