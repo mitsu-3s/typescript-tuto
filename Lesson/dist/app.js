8bf697e1825ab626f72b17b28068d82e86076185
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-02-13 20:01:01 +0900

    During Lesson

--- Lesson/dist/app.js
+++ Lesson/dist/app.js
@@ -2,3 +2,24 @@
 const userName = 'Max';
 let age = 30;
 age = 29;
+const add = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+const button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', (event) => {
+        console.log(event);
+    });
+}
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking', ...hobbies];
+activeHobbies.push(...hobbies);
+const person = {
+    name: 'Max',
+    age: 30,
+};
+const copiedPerson = Object.assign({}, person);
--- Lesson/src/app.ts
+++ Lesson/src/app.ts
@@ -1,4 +1,54 @@
 const userName = 'Max'
-let age = 30;
+// userName = 'Min'
+let age = 30
 
-age = 29;
+age = 29
+
+// function add(a: number, b: number) {
+//     let result
+//     result = a + b
+//     return result
+// }
+
+// const add = (a: number, b: number) => {
+//     return a + b
+// }
+
+// const add = (a: number, b: number = 1) => a + b
+
+// const printOutput: (output: string | number) => void = (output) => {
+//     console.log(output)
+// }
+
+// printOutput(add(2))
+
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue
+    }, 0)
+}
+
+const addedNumbers = add(5, 10, 2, 3.7)
+console.log(addedNumbers)
+
+const button = document.querySelector('button')
+
+if (button) {
+    button.addEventListener('click', (event) => {
+        console.log(event)
+    })
+}
+
+const hobbies = ['Sports', 'Cooking']
+const activeHobbies = ['Hiking', ...hobbies]
+
+activeHobbies.push(...hobbies)
+
+const person = {
+    name: 'Max',
+    age: 30,
+}
+
+const copiedPerson = {
+    ...person,
+}
