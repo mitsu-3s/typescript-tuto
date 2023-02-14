c8f0baa0ef3d079afffa33bf4074ceb8185c30f6
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-02-15 06:27:39 +0900

    During Lesson

--- Lesson/dist/app.js
+++ Lesson/dist/app.js
@@ -1,120 +1,94 @@
-'use strict'
-var __decorate =
-    (this && this.__decorate) ||
-    function (decorators, target, key, desc) {
-        var c = arguments.length,
-            r =
-                c < 3
-                    ? target
-                    : desc === null
-                    ? (desc = Object.getOwnPropertyDescriptor(target, key))
-                    : desc,
-            d
-        if (
-            typeof Reflect === 'object' &&
-            typeof Reflect.decorate === 'function'
-        )
-            r = Reflect.decorate(decorators, target, key, desc)
-        else
-            for (var i = decorators.length - 1; i >= 0; i--)
-                if ((d = decorators[i]))
-                    r =
-                        (c < 3
-                            ? d(r)
-                            : c > 3
-                            ? d(target, key, r)
-                            : d(target, key)) || r
-        return c > 3 && r && Object.defineProperty(target, key, r), r
-    }
-var __param =
-    (this && this.__param) ||
-    function (paramIndex, decorator) {
-        return function (target, key) {
-            decorator(target, key, paramIndex)
-        }
-    }
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Logger(logString) {
-    console.log('LOGGER factory')
+    console.log('LOGGER factory');
     return function (constructor) {
-        console.log(logString)
-        console.log(constructor)
-    }
+        console.log(logString);
+        console.log(constructor);
+    };
 }
 function WithTemplate(template, hookId) {
-    console.log('TEMPLATE factory')
+    console.log('TEMPLATE factory');
     return function (originalConstructor) {
         return class extends originalConstructor {
             constructor(..._) {
-                super()
-                console.log('Display template')
-                const hookEl = document.getElementById(hookId)
+                super();
+                console.log('Display template');
+                const hookEl = document.getElementById(hookId);
                 if (hookEl) {
-                    hookEl.innerHTML = template
-                    hookEl.querySelector('h1').textContent = this.name
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1').textContent = this.name;
                 }
             }
-        }
-    }
+        };
+    };
 }
 let Person = class Person {
     constructor() {
-        this.name = 'Max'
-        console.log('Creating a Person object')
+        this.name = 'Max';
+        console.log('Creating a Person object');
     }
-}
-Person = __decorate(
-    [
-        Logger('Log output in progress'),
-        WithTemplate('<h1>Person Object</h1>', 'app'),
-    ],
-    Person
-)
-const pers = new Person()
-console.log(pers)
+};
+Person = __decorate([
+    Logger('Log output in progress'),
+    WithTemplate('<h1>Person Object</h1>', 'app')
+], Person);
+const pers = new Person();
+console.log(pers);
 function Log(target, propertyName) {
-    console.log('Property decorator')
-    console.log(target, propertyName)
+    console.log('Property decorator');
+    console.log(target, propertyName);
 }
 function Log2(target, name, descriptor) {
-    console.log('Accessor decorator')
-    console.log(target)
-    console.log(name)
-    console.log(descriptor)
+    console.log('Accessor decorator');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
 }
 function Log3(target, name, descriptor) {
-    console.log('Method decorator')
-    console.log(target)
-    console.log(name)
-    console.log(descriptor)
+    console.log('Method decorator');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
 }
 function Log4(target, name, position) {
-    console.log('Parameter decorator')
-    console.log(target)
-    console.log(name)
-    console.log(position)
+    console.log('Parameter decorator');
+    console.log(target);
+    console.log(name);
+    console.log(position);
 }
 class Product {
     set price(val) {
         if (val > 0) {
-            this._price = val
+            this._price = val;
         }
-        throw new Error('Incorrect price, cannot be set below 0.')
+        throw new Error('Incorrect price, cannot be set below 0.');
     }
     constructor(t, p) {
-        this.title = t
-        this._price = p
+        this.title = t;
+        this._price = p;
     }
     getPricewWithTax(tax) {
-        return this._price * (1 + tax)
+        return this._price * (1 + tax);
     }
 }
-__decorate([Log], Product.prototype, 'title', void 0)
-__decorate([Log2], Product.prototype, 'price', null)
-__decorate(
-    [Log3, __param(0, Log4)],
-    Product.prototype,
-    'getPricewWithTax',
-    null
-)
-const p1 = new Product('Book', 100)
-const p2 = new Product('Book2', 200)
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPricewWithTax", null);
+const p1 = new Product('Book', 100);
+const p2 = new Product('Book2', 200);
