04409c2785aeaa22f52cd0adbd8b8a701a7dc89b
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-02-14 16:27:14 +0900

    During Lesson

--- Lesson/src/app.ts
+++ Lesson/src/app.ts
@@ -48,6 +48,12 @@ const fetchedUserData = {
 
 console.log(fetchedUserData?.job?.title)
 
+const userInput = undefined
+
+const storedData = userInput ?? 'DEFAULT'
+
+console.log(storedData)
+
 // type UnknownEmployee = Employee | Admin
 
 // function printEmployeeInformation(emp: UnknownEmployee) {
