07194b4b1194f907a0e63bb7a177bf608feee5b7
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-02-16 06:50:50 +0900

    During Lesson

--- Lesson/src/app.ts
+++ Lesson/src/app.ts
@@ -1,3 +1,51 @@
+// Validation
+interface Validatable {
+    value: string | number
+    required?: boolean
+    minLength?: number
+    maxLength?: number
+    min?: number
+    max?: number
+}
+
+function validate(validatableInput: Validatable) {
+    let isValid = true
+    if (validatableInput.required) {
+        isValid =
+            isValid && validatableInput.value.toString().trim().length !== 0
+    }
+    if (
+        validatableInput.minLength != null &&
+        typeof validatableInput.value === 'string'
+    ) {
+        isValid =
+            isValid &&
+            validatableInput.value.length >= validatableInput.minLength
+    }
+    if (
+        validatableInput.maxLength != null &&
+        typeof validatableInput.value === 'string'
+    ) {
+        isValid =
+            isValid &&
+            validatableInput.value.length <= validatableInput.maxLength
+    }
+
+    if (
+        validatableInput.min != null &&
+        typeof validatableInput.value === 'number'
+    ) {
+        isValid = isValid && validatableInput.value >= validatableInput.min
+    }
+    if (
+        validatableInput.max != null &&
+        typeof validatableInput.value === 'number'
+    ) {
+        isValid = isValid && validatableInput.value <= validatableInput.max
+    }
+    return isValid
+}
+
 // autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value
@@ -51,16 +99,38 @@ class ProjectInput {
         const enteredTitle = this.titleInputElement.value
         const enteredDescription = this.descriptionInputElement.value
         const enteredManday = this.mandayInputElement.value
+
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true,
+        }
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5,
+        }
+        const mandayValidatable: Validatable = {
+            value: enteredManday,
+            required: true,
+            min: 1,
+            max: 1000,
+        }
         if (
-            enteredTitle.trim().length === 0 ||
-            enteredDescription.trim().length === 0 ||
-            enteredManday.trim().length === 0
+            !validate(titleValidatable) &&
+            !validate(descriptionValidatable) &&
+            !validate(mandayValidatable)
         ) {
             alert('Input value is incorrect. Please try again.')
             return
         } else {
             return [enteredTitle, enteredDescription, +enteredManday]
         }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = ''
+        this.descriptionInputElement.value = ''
+        this.mandayInputElement.value = ''
     }
 
     @autobind
@@ -70,6 +140,7 @@ class ProjectInput {
         if (Array.isArray(userInput)) {
             const [title, desc, manday] = userInput
             console.log(title, desc, manday)
+            this.clearInputs()
         }
     }
 
